407a50a2b2ee2049cb7202bd30dd11b6eb1e936b
Andrew Rutter <andlrutt@vols.utk.edu>
2023-10-16 00:40:40 -0400

    updated exports

--- src/db/models/FormBuilder/index.ts
+++ src/db/models/FormBuilder/index.ts
@@ -1,6 +1,3 @@
-export { default as FileUploadQuestionSchema } from './FileUploadQuestion';
-export { default as FormSchema } from './Form';
-export { default as FormResponseSchema } from './FormResponse';
-export { default as MultipleChoiceQuestionSchema } from './MultipleChoiceQuestion';
-export { default as NumericQuestionSchema } from './NumericQuestion';
-export { default as TextQuestionSchema } from './TextQuestion';
+export { default as FormSchema } from "./Form";
+export { default as FormQuestionSchema } from "./FormQuestion";
+export { default as FormResponseSchema } from "./FormResponse";
--- src/types/FormBuilder/index.ts
+++ src/types/FormBuilder/index.ts
@@ -1,2 +1,3 @@
-export * from './form';
-export * from './formResponse';
+export * from "./form";
+export * from "./formQuestion";
+export * from "./formResponse";
